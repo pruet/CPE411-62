a53027e023b46c992a695592563a7cfbb9c826be
fluke <thanakom.fluke@gmail.com>
2020-03-06 14:12:22 +0700

    un do commit hw2
    wrong push branch

--- StatPlugin.cs
+++ StatPlugin.cs
@@ -7,7 +7,6 @@ namespace DNWS
 {
   class StatPlugin : IPlugin
   {
-    public static Mutex MuTexLock = new Mutex(); 
     protected static Dictionary<String, int> statDictionary = null;
     public StatPlugin()
     {
@@ -20,7 +19,6 @@ namespace DNWS
 
     public void PreProcessing(HTTPRequest request)
     {
-      MuTexLock.WaitOne();
       if (statDictionary.ContainsKey(request.Url))
       {
         statDictionary[request.Url] = (int)statDictionary[request.Url] + 1;
@@ -29,14 +27,11 @@ namespace DNWS
       {
         statDictionary[request.Url] = 1;
       }
-      MuTexLock.ReleaseMutex();
     }
     public HTTPResponse GetResponse(HTTPRequest request)
     {
       HTTPResponse response = null;
       StringBuilder sb = new StringBuilder();
-      MuTexLock.WaitOne(); 
-      // Console.WriteLine("IncThread acquires the mutex.");  
       sb.Append("<html><body><h1>Stat:</h1>");
       foreach (KeyValuePair<String, int> entry in statDictionary)
       {
@@ -45,8 +40,6 @@ namespace DNWS
       sb.Append("</body></html>");
       response = new HTTPResponse(200);
       response.body = Encoding.UTF8.GetBytes(sb.ToString());
-      // Console.WriteLine("IncThread releases the mutex."); 
-      MuTexLock.ReleaseMutex(); 
       return response;
     }
 
