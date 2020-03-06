e144081629ee60dd95ec418a88930b2844885ec4
sasirat maneejian <sasirat019@gmail.com>
2020-03-06 18:08:29 +0700

    Add Mutex in StatPlugin

--- StatPlugin.cs
+++ StatPlugin.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace DNWS
 {
   class StatPlugin : IPlugin
   {
     protected static Dictionary<String, int> statDictionary = null;
+
+    protected static Mutex mutex = null;
     public StatPlugin()
     {
       if (statDictionary == null)
@@ -14,10 +17,15 @@ namespace DNWS
         statDictionary = new Dictionary<String, int>();
 
       }
+      if (mutex == null)
+      {
+        mutex = new Mutex();
+      }
     }
 
     public void PreProcessing(HTTPRequest request)
     {
+      mutex.WaitOne();
       if (statDictionary.ContainsKey(request.Url))
       {
         statDictionary[request.Url] = (int)statDictionary[request.Url] + 1;
@@ -26,13 +34,15 @@ namespace DNWS
       {
         statDictionary[request.Url] = 1;
       }
+      mutex.ReleaseMutex();
     }
     public HTTPResponse GetResponse(HTTPRequest request)
     {
       HTTPResponse response = null;
       StringBuilder sb = new StringBuilder();
+      Dictionary<String, int> copyDictionary = new Dictionary<String, int>(statDictionary);
       sb.Append("<html><body><h1>Stat:</h1>");
-      foreach (KeyValuePair<String, int> entry in statDictionary)
+      foreach (KeyValuePair<String, int> entry in copyDictionary)
       {
         sb.Append(entry.Key + ": " + entry.Value.ToString() + "<br />");
       }
